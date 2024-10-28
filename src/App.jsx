e175172695491d0ddb6b5a30bbbffe651b5ba593
Diyor <>
2024-10-28 20:14:40 +0500

    react project template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CakeCont from "./components/CakeCont";
+
+const App = () => {
+    return (
+        <div>
+            <CakeCont />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/CakeCont.jsx
@@ -0,0 +1,1 @@
+import React from "react";const CakeCont = () => {    return (        <div>            <h1>Number of Cake</h1>            <button>buy cake</button>        </div>    );};export default CakeCont;
