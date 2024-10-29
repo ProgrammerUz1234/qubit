b2dc143736050ca3bb88f70f2201e0ba75e893fb
Diyor <>
2024-10-29 21:48:21 +0500

    grow page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Main from "./components/Main/Main.jsx";
+import Customers from "./components/Customers/Customers.jsx";
+import Grow from "./components/Grow/Grow.jsx";
 
 const App = () => {
     return (
         <div>
             <Navbar />
             <Main />
+            <Customers />
+            <Grow />
         </div>
     );
 };
